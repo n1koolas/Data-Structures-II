2cfbaf5811d53450ca882ae540123e3d9b497a81
feroferofero <nfreitassilva5@outlook.com>
2025-06-17 15:14:44 -0300

    update folders.

--- search&compression/random_number_bs/src/Main.java
+++ search&compression/random_number_bs/src/Main.java
@@ -1,5 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*.Implement binary search to find all occurrences of a specific value within the collection
+    - Display the number of times the value appeared and the positions where it was found.
+    - Display the number of comparisons that were performed.*/
+
 public class Main {
     public static void main(String[] args) {
         Array array = new Array();
